8d1e75d1b6c64c6f7cac3c60b9de53f19bf90bbc
XCK1998 <b17040217@njupt.edu.cn>
2019-11-27 10:25:42 +0800

    修复小程序登录授权bug

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,6 @@
 // pages/index/index.js
 const app = getApp();
+const db = wx.cloud.database()
 Page({
 
   /**
@@ -14,7 +15,8 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('view.open-type.getUserInfo')
   },
-  start: function (e) {
+  start: function(e) {
+    /*
     if (wx.getStorageSync('userinfo')) {
       this.setData({
         userInfo: wx.getStorageSync('userinfo'),
@@ -23,42 +25,73 @@ Page({
       wx.navigateTo({
         url: '../list/list',
       })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    } */
+    db.collection('UserInfo').where({
+      _openid: wx.getStorageSync('_OPENID')
+    }).get().then(res => {
+      if (res.data.length > 0) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          userInfo: res.data[0].userInfo,
+          hasUserInfo: true,
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+        wx.setStorageSync('userinfo', this.data.userInfo)
+        wx.navigateTo({
+          url: '../list/list',
+        })
+      } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
-      })
-    }
-    if (!wx.getStorageSync('userinfo')) {
-      wx.showModal({
-        title: '提示',
-        content: '您还没有注册',
-        showCancel: false,
-        confirmText: "确定",
-        success: function (res) {
-          wx.navigateTo({
-            url: '../userlogin/userlogin',
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true
+            })
+          }
+        })
+      }
+      db.collection('UserInfo').where({
+        _openid: wx.getStorageSync('_OPENID')
+      }).get().then(res => {
+        if (res.data.length <= 0) {
+          wx.showModal({
+            title: '提示',
+            content: '您还没有注册',
+            showCancel: false,
+            confirmText: "确定",
+            success: function(res) {
+              wx.navigateTo({
+                url: '../userlogin/userlogin',
+              })
+            }
           })
         }
       })
-    }
+    })
+    /*  if (!wx.getStorageSync('userinfo')) {
+        wx.showModal({
+          title: '提示',
+          content: '您还没有注册',
+          showCancel: false,
+          confirmText: "确定",
+          success: function (res) {
+            wx.navigateTo({
+              url: '../userlogin/userlogin',
+            })
+          }
+        })
+      }*/
   },
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     wx.navigateTo({
       url: '../userlogin/userlogin',
     })
@@ -71,53 +104,53 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) { },
+  onLoad: function(options) {},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 })
