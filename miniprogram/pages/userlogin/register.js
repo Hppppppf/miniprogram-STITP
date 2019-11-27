24cf8d4f343759d1ddb50894846e5c9b7f99da73
Hppppppf <1169128383@qq.com>
2019-11-27 10:24:28 +0800

    尝试修复位置授权

--- miniprogram/pages/userlogin/register.js
+++ miniprogram/pages/userlogin/register.js
@@ -167,7 +167,45 @@ Page({
       detail: e.detail.value,
     })
   },
+  openMap: function (e) {
+    var that = this
+    wx.getSetting({
+      success(res) {
+        //这里判断是否有地位权限
+        if (!res.authSetting['scope.userLocation']) {
+          wx.showModal({
+            title: '提示',
+            content: '请求获取位置权限',
+            success: function (res) {
+              if (res.confirm == false) {
+                return false;
+              }
+              wx.openSetting({
+                success(res) {
+                  //如果再次拒绝则返回页面并提示
+                  if (!res.authSetting['scope.userLocation']) {
+                    wx.showToast({
+                      title: '此功能需获取位置信息，请重新设置',
+                      duration: 3000,
+                      icon: 'none'
+                    })
+                  } else {
+                    //允许授权，调用地图
+                    that.getGeopoint()
+                  }
+                }
+              })
+            }
+          })
+        } else {
+          //如果有定位权限，调用地图
+          that.getGeopoint()
+        }
 
+      }
+
+    })
+  },
   getGeopoint: function() {
     var that = this
     wx.chooseLocation({
